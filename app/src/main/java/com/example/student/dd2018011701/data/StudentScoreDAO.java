7fa6975f0823af7d9d2b7df8c9da776ee135ac6c
kcli2412 <kcli2412@gmail.com>
2018-01-17 10:59:34 +0800

    Student & StudentScoreDAO

---
+++ app/src/main/java/com/example/student/dd2018011701/data/StudentScoreDAO.java
@@ -0,0 +1,40 @@
+package com.example.student.dd2018011701.data;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Student on 2018/1/17.
+ */
+
+public class StudentScoreDAO {
+    ArrayList<Student> mylist;
+
+    public StudentScoreDAO()
+    {
+        mylist = new ArrayList<>();
+    }
+
+    public boolean add(Student s)
+    {
+        mylist.add(s);
+
+        return true;
+    }
+
+    public ArrayList<Student> getList()
+    {
+        return mylist;
+    }
+
+    public Student getStudent(int id)
+    {
+        for (Student s:mylist)
+        {
+            if (s.id == id)
+            {
+                return s;
+            }
+        }
+        return null;
+    }
+}
--- app/src/test/java/com/example/student/dd2018011701/ExampleUnitTest.java
+++ app/src/test/java/com/example/student/dd2018011701/ExampleUnitTest.java
@@ -1,5 +1,8 @@
 package com.example.student.dd2018011701;
 
+import com.example.student.dd2018011701.data.Student;
+import com.example.student.dd2018011701.data.StudentScoreDAO;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,20 +13,36 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void test_add_data() throws Exception {
+        StudentScoreDAO dao = new StudentScoreDAO();
+        dao.add(new Student(1,"Bob",95));
+        dao.add(new Student(2,"Mary",80));
+        assertEquals(2, dao.getList().size());
     }
 
     @Test
-    public void test1() throws Exception {
-        assertEquals(6, 2 + 4);
+    public void test_add_data2() throws Exception {
+        StudentScoreDAO dao = new StudentScoreDAO();
+        dao.add(new Student(1,"Bob",95));
+        dao.add(new Student(2,"Mary",80));
+        assertEquals(80,dao.getList().get(1).score);
     }
 
     @Test
-    public void test2() throws Exception {
-        MyTest1 t1 = new MyTest1();
+    public void test_getStudent() throws Exception {
+        StudentScoreDAO dao = new StudentScoreDAO();
+        dao.add(new Student(1,"Bob",95));
+        dao.add(new Student(2,"Mary",80));
+        assertEquals(80, dao.getStudent(2).score);
+    }
 
-        assertEquals(8, t1.getAdd(2, 6));
+    @Test
+    public void test_getStudent1() throws Exception {
+        StudentScoreDAO dao = new StudentScoreDAO();
+        dao.add(new Student(1,"Bob",95));
+        dao.add(new Student(2,"Mary",80));
+        assertEquals(null, dao.getStudent(3));
     }
 }
